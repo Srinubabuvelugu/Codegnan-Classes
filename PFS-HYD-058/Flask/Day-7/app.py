c9c8f158fb1490585167d91cb77304323ffff9a1
Srinubabuvelugu <srinubabu1911@gmail.com>
2026-09-21 10:57:28 +0530

    Home and Register route implemented

--- PFS-HYD-058/Flask/Day-7/app.py
+++ PFS-HYD-058/Flask/Day-7/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 from database import createTables
 
@@ -9,7 +9,42 @@ app = Flask(__name__)
 # home route
 @app.route("/")
 def home():
-    return "This is home page"
+    return render_template('home.html')
+
+
+# Register route
+@app.route('/register', methods = ['GET','POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+        print(email, name, password, confirm_password)
+
+        # check password and confirm_password both are same or not
+        #check user email already exists or not
+        # redirect tot Verifyotp page
+
+
+
+
+
+
+#verify otp route
+@app.route('/verifyotp', methods = ['GET','POST'])
+def verifyotp():
+    if request.method == 'GET':
+        return render_template('verifyotp.html')
+
+# Login Route
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
 
 
 
